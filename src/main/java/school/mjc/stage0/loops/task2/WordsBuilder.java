a35005d2a2c565949b6e5fbb0a1a534216db5fd5
AlionaVAHA <108309355+AlionaVAHA@users.noreply.github.com>
2023-06-16 16:43:33 -0400

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-
+     int counter = 0;
+        while (counter< args.length){
+            System.out.println(args[counter]);
+            counter++;
+        }
     }
 }
